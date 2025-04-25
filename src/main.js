bff9a0c56e521d80f29f057d0d44924895ec7854
Vlad Mavrin <zhilenkovlad1997@gmail.com>
2025-04-25 21:22:40 +0100

    fix js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-// import './js/api.js';
-// import './js/header.js';
-// import './js/faq.js';
-// import './js/reviews.js';
-// import './js/work-together.js';
+import './js/api.js';
+import './js/header.js';
+import './js/faq.js';
+import './js/reviews.js';
+import './js/work-together.js';
