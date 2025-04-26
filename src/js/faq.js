88b3d08dca0bab46e996409cf3840241855f9f28
Daria Makivska <peony.bluet@gmail.com>
2025-04-26 15:36:37 +0300

    Додає html, css, js mobile

--- src/js/faq.js
+++ src/js/faq.js
@@ -2,37 +2,52 @@ import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
 const faqAccordion = new Accordion('.faq-list', {
-  duration: 400,
+  duration: 300,
   showOne: true,
-  onToggle: function (event) {
+  openOnInit: [0],
+  onOpen: function (currentElement) {
     const allItems = document.querySelectorAll('.faq-item');
+
     allItems.forEach(function (item) {
       item.classList.remove('ac-active');
+
+      const svgUse = item.querySelector('.faq-btn-accordion use');
+      if (svgUse) {
+        svgUse.setAttribute('href', 'img/sprite.svg#icon-faq-down');
+      }
     });
+
+    const openedItem = currentElement.closest('.faq-item');
+
+    if (openedItem) {
+      openedItem.classList.add('ac-active');
+
+      const svgUse = openedItem.querySelector('.faq-btn-accordion use');
+      if (svgUse) {
+        svgUse.setAttribute('href', 'img/sprite.svg#icon-faq-up');
+      }
+    }
+  },
+  onClose: function (currentElement) {
+    const closedItem = currentElement.closest('.faq-item');
+    if (closedItem) {
+      closedItem.classList.remove('ac-active');
+    }
   },
 });
 
-const faqItems = document.querySelectorAll('.faq-item');
+const faqTriggers = document.querySelectorAll('.faq-btn-accordion');
 
-function toggleActiveClass(event) {
-  const item = event.target.closest('.faq-item');
-  if (item) {
-    closeAllItems();
+faqTriggers.forEach(function (btn) {
+  btn.addEventListener('click', function () {
+    const svgUse = btn.querySelector('use');
+    const parentItem = btn.closest('.faq-item');
+    const isOpen = parentItem.classList.contains('ac-active');
 
-    item.classList.add('ac-active');
-  }
-}
-
-function closeAllItems() {
-  faqItems.forEach(function (i) {
-    i.classList.remove('ac-active');
+    if (isOpen) {
+      svgUse.setAttribute('href', 'img/sprite.svg#icon-faq-down');
+    } else {
+      svgUse.setAttribute('href', 'img/sprite.svg#icon-faq-up');
+    }
   });
-}
-
-faqItems.forEach(function (item) {
-  const button = item.querySelector('.faq-btn-accordion');
-  button.addEventListener('click', toggleActiveClass);
 });
-
-faqItems[0].classList.add('ac-active');
-faqItems[0].querySelector('.faq-answer').style.display = 'block'; // Показуємо відповідь першого елемента
